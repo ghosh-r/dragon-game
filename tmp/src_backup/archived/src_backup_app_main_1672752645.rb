acf5a5cb40b34ae24707ba349f3df66be8844135
Rito Ghosh <coder.rg7@gmail.com>
2023-01-03 19:55:26 +0530

    all arrays replaced with hashed

---
+++ tmp/src_backup/archived/src_backup_app_main_1672752645.rb
@@ -0,0 +1,73 @@
+def tick args
+  args.state.player ||= {
+    x: 120,
+    y: 280,
+    w: 100,
+    h: 80,
+    speed: 10,
+    path: 'sprites/misc/dragon-0.png'
+  }
+
+  args.state.fireballs ||= []
+
+  if args.inputs.left and args.inputs.up
+    args.state.player.x -= args.state.player.speed / 2
+    args.state.player.y += args.state.player.speed / 2
+  end
+
+  if args.inputs.left and args.inputs.down
+    args.state.player.x -= args.state.player.speed / 2
+    args.state.player.y -= args.state.player.speed / 2
+  end
+
+  if args.inputs.right and args.inputs.up
+    args.state.player.x += args.state.player.speed / 2
+    args.state.player.y += args.state.player.speed / 2
+  end
+
+  if args.inputs.right and args.inputs.down
+    args.state.player.x += args.state.player.speed / 2
+    args.state.player.y -= args.state.player.speed / 2
+  end
+
+  if args.inputs.left
+    args.state.player.x -= args.state.player.speed
+  elsif args.inputs.right
+    args.state.player.x += args.state.player.speed
+  end
+
+  if args.inputs.up
+    args.state.player.y += args.state.player.speed
+  elsif args.inputs.down
+    args.state.player.y -= args.state.player.speed
+  end
+
+  if args.state.player.x + args.state.player.w > args.grid.w
+    args.state.player.x = args.grid.w - player_w
+  end
+  
+  if args.state.player_x < 0
+    args.state.player_x = 0
+  end
+
+  if args.state.player_y + player_h > args.grid.h
+    args.state.player_y = args.grid.h - player_h
+  end
+
+  if args.state.player_y < 0
+    args.state.player_y = 0
+  end
+
+  if args.inputs.keyboard.key_down.z ||
+      args.inputs.keyboard.key_down.j ||
+      args.inputs.controller_one.key_down.a
+    args.state.fireballs << [args.state.player_x, args.state.player_y, 'fireball']
+  end
+
+  args.state.fireballs.each do |fireball|
+    fireball[0] += speed + 2
+  end
+
+  args.outputs.sprites << [args.state.player_x, args.state.player_y, player_w, player_h, 'sprites/misc/dragon-0.png']
+  args.outputs.labels << args.state.fireballs
+end
--- tmp/src_backup/src_backup_app_main.rb
+++ tmp/src_backup/src_backup_app_main.rb
@@ -1,69 +1,73 @@
 def tick args
-  args.state.player_x ||= 120
-  args.state.player_y ||= 280
+  args.state.player ||= {
+    x: 120,
+    y: 280,
+    w: 100,
+    h: 80,
+    speed: 10,
+    path: 'sprites/misc/dragon-0.png'
+  }
+
   args.state.fireballs ||= []
-  speed = 10
-  player_w = 100
-  player_h = 80
 
   if args.inputs.left and args.inputs.up
-    args.state.player_x -= speed / 2
-    args.state.player_y += speed / 2
+    args.state.player.x -= args.state.player.speed / 2
+    args.state.player.y += args.state.player.speed / 2
   end
 
   if args.inputs.left and args.inputs.down
-    args.state.player_x -= speed / 2
-    args.state.player_y -= speed / 2
+    args.state.player.x -= args.state.player.speed / 2
+    args.state.player.y -= args.state.player.speed / 2
   end
 
   if args.inputs.right and args.inputs.up
-    args.state.player_x += speed / 2
-    args.state.player_y += speed / 2
+    args.state.player.x += args.state.player.speed / 2
+    args.state.player.y += args.state.player.speed / 2
   end
 
   if args.inputs.right and args.inputs.down
-    args.state.player_x += speed / 2
-    args.state.player_y -= speed / 2
+    args.state.player.x += args.state.player.speed / 2
+    args.state.player.y -= args.state.player.speed / 2
   end
 
   if args.inputs.left
-    args.state.player_x -= speed
+    args.state.player.x -= args.state.player.speed
   elsif args.inputs.right
-    args.state.player_x += speed
+    args.state.player.x += args.state.player.speed
   end
 
   if args.inputs.up
-    args.state.player_y += speed
+    args.state.player.y += args.state.player.speed
   elsif args.inputs.down
-    args.state.player_y -= speed
+    args.state.player.y -= args.state.player.speed
   end
 
-  if args.state.player_x + player_w > args.grid.w
-    args.state.player_x = args.grid.w - player_w
+  if args.state.player.x + args.state.player.w > args.grid.w
+    args.state.player.x = args.grid.w - args.state.player.w
   end
   
-  if args.state.player_x < 0
-    args.state.player_x = 0
+  if args.state.player.x < 0
+    args.state.player.x = 0
   end
 
-  if args.state.player_y + player_h > args.grid.h
-    args.state.player_y = args.grid.h - player_h
+  if args.state.player.y + args.state.player.h > args.grid.h
+    args.state.player.y = args.grid.h - args.state.player.h
   end
 
-  if args.state.player_y < 0
-    args.state.player_y = 0
+  if args.state.player.y < 0
+    args.state.player.y = 0
   end
 
   if args.inputs.keyboard.key_down.z ||
       args.inputs.keyboard.key_down.j ||
       args.inputs.controller_one.key_down.a
-    args.state.fireballs << [args.state.player_x, args.state.player_y, 'fireball']
+    args.state.fireballs << [args.state.player.x, args.state.player.y, 'fireball']
   end
 
   args.state.fireballs.each do |fireball|
-    fireball[0] += speed + 2
+    fireball[0] += args.state.player.speed + 2
   end
 
-  args.outputs.sprites << [args.state.player_x, args.state.player_y, player_w, player_h, 'sprites/misc/dragon-0.png']
+  args.outputs.sprites << [args.state.player.x, args.state.player.y, args.state.player.w, args.state.player.h, 'sprites/misc/dragon-0.png']
   args.outputs.labels << args.state.fireballs
 end
